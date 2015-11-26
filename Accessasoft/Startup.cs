4c2d86fd3e637f52226a9e529dddeb8bc81052b0
tblokzyl <tblokzyl@gmail.com>
2015-11-26 15:09:54 -0500

    Initial_Push (Solution)

---
+++ Accessasoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Accessasoft.Startup))]
+namespace Accessasoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
